f05edf9b7b769c3cdba967b604e4a4422872d41a
NEBULA1978 <rpascualey@gmail.com>
2023-08-14 19:18:43 +0200

    Final video 2 falta en home divido columnas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,13 @@
     <h1>Home</h1>
     <div class="row">
       <div class="col-6">
-        <img alt="Vue logo" src="../assets/img/6735952.jpg">
-
+        <img alt="Vue logo" src="@/assets/model-3.avif" style="width: 100%; max-height: 100%; object-fit: cover;">
       </div>
       <div class="col-6">
-        
+        <!-- Contenido de la columna derecha -->
       </div>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -18,3 +17,5 @@
 // @ is an alias to /src
 
 </script>
+
+<style></style>
