16d7f06f7728c5bc8ffcbad7e4ea5d0427f9cd01
NEBULA1978 <rpascualey@gmail.com>
2023-08-14 02:50:10 +0200

    Introduzco iconos en Navbar cambiando por texto

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,33 +1,43 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <router-link router-link to="/" class="navbar-brand">LOGO</router-link><routes-link></routes-link>
+            <router-link router-link to="/" class="navbar-brand">
+                <span class="material-icons">home</span>
+            </router-link><routes-link></routes-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>
-
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/about" class="nav-link active" aria-current="page">About</router-link>
-
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/posts" class="nav-link active" aria-current="page">Posts</router-link>
-
-                    </li>
-                    <li>
-                        <span class="material-icons">
-                            <span class="material-symbols-outlined">
-                                settings
+                    <!-- <li>
+                            <span class="material-icons">
+                                <span class="material-symbols-outlined">
+                                    settings
+                                </span>
+                                home
                             </span>
-                            home
-                        </span>
+                        </li> -->
+
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link active" aria-current="page">
+                            <span class="material-icons">home</span>
+                        </router-link>
+
                     </li>
+                    <li class="nav-item">
+                        <router-link to="/about" class="nav-link active" aria-current="page">
+                            <span class="material-icons">info</span>
+                        </router-link>
+
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/posts" class="nav-link active" aria-current="page">
+                            <span class="material-icons">featured_play_list</span>
+                        </router-link>
+
+                    </li>
+                    
 
                 </ul>
             </div>
