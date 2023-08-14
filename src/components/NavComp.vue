c920087b033ae818411df2e5a4997fbbfedbe557
NEBULA1978 <rpascualey@gmail.com>
2023-08-14 18:25:13 +0200

    Dando color a logos Navbar en archivo NavComp

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,47 +1,50 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <router-link router-link to="/" class="navbar-brand">
-                <!-- <span class="material-icons">home</span> -->
-            </router-link><routes-link></routes-link>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container justify-content-eventy">
+            <router-link to="/" class="navbar-brand">
+                <span class="material-icons">home</span>
+            </router-link>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <!-- <li>
-                            <span class="material-icons">
-                                <span class="material-symbols-outlined">
-                                    settings
-                                </span>
-                                home
-                            </span>
-                        </li> -->
 
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link active" aria-current="page">
-                            <span class="material-icons">home</span>
-                        </router-link>
 
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/about" class="nav-link active" aria-current="page">
-                            <span class="material-icons">info</span>
-                        </router-link>
+                <router-link to="/" class="nav-link active" aria-current="page">
+                    <span class="material-icons-round">home</span>
+                </router-link>
 
-                    </li>
-                    <li class="nav-item">
-                        <!-- Sustituimos la a del link html por router-link -->
-                        <router-link to="/posts" class="nav-link active" aria-current="page">
-                            <span class="material-icons">featured_play_list</span>
-                        </router-link>
 
-                    </li>
-                    
+                <router-link to="/about" class="nav-link active" aria-current="page">
+                    <span class="material-icons-round">info</span>
+                </router-link>
 
-                </ul>
+
+                <router-link to="/posts" class="nav-link active" aria-current="page">
+                    <span class="material-icons-round">featured_play_list</span>
+                </router-link>
+
+                <!-- ... otros elementos ... -->
+
             </div>
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+    name: "Navcom"
+}
+</script>
+
+<style>
+.material-icons-round {
+    color: white;
+    font-size: 1.4em;
+    background-color: grey;
+    padding: 0.3em;
+    border-radius: 50%;
+}
+</style>
